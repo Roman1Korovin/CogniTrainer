3fc4079ee731ccf149aab4f9d0c452c75ce56cdb
dazzer <roman.korovin.2018@mail.ru>
2025-05-05 20:08:56 +0700

    Add base implementation of ReactionTrainer plugin
    - Created ReactionTrainer as a separate C++ library project integrated
    into the main CogniTrainer project
    - Implemented basic plugin interface with QML component support
    - Configured ReactionTrainer to build as a DLL using add_subdirectory in
    the main CMakeLists.txt
    - Set output path for the generated DLL to the 'modules' directory in
    the root of the project

---
+++ plugins/ReactionTrainer/ReactionTrainer.h
@@ -0,0 +1,24 @@
+#ifndef REACTIONTRAINER_H
+#define REACTIONTRAINER_H
+
+#include <QObject>
+#include "../../interfaces/ModuleInterface.h"
+
+class ReactionTrainer : public QObject, public ModuleInterface
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID ModuleInterface_iid)
+    Q_INTERFACES(ModuleInterface)
+
+public:
+    QString name() const override {
+        return QStringLiteral("Тренировка реакции");
+    }
+    QString description() const override {
+        return QStringLiteral("Упражнение на проверку скорости реакции");
+    }
+    QUrl qmlComponentUrl() const override {
+        return QUrl("qrc:/ReactionTrainer.qml");
+    }
+};
+#endif // REACTIONTRAINER_H
---
+++ plugins/ReactionTrainer/ReactionTrainer.qml
@@ -0,0 +1,14 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+
+Rectangle {
+    width: 400; height: 300
+    color: "lightsteelblue"
+
+    Text {
+        anchors.centerIn: parent
+        text: "Упражнение: реакция"
+        font.pointSize: 24
+    }
+    // тут потом будет сама логика тренировки
+}
